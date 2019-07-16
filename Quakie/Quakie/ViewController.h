a617d81ca646f344f33cd33b69b14f9085a9e0c2
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-07-16 12:11:05 -0400

    add starter Xcode project

---
+++ Quakie/Quakie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quakie
+//
+//  Created by Julian A. Fordyce on 7/16/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
